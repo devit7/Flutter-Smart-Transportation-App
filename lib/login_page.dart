46b519bb5447e0abafe2c312313cf7f9647643b2
dnabilpc <dnabilpc@gmail.com>
2024-12-28 17:09:59 +0700

    add flutter svg, add logo busstop asset, add login_page.dart

---
+++ lib/login_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        
+        children: [
+          Text("BusStop",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'HankenGrotesk',
+                fontSize: 48,
+                fontWeight: FontWeight.w800,
+
+              )
+          ),
+        ],
+      ),
+    );
+  }
+}
