59e5df99170ccc98311b26fb3a04436c06bc4f35
rickypratama17 <mrickyp28@gmail.com>
2025-01-06 13:06:32 +0700

    api dashboard

--- lib/Model/penumpang_api_model.dart
+++ lib/Model/penumpang_api_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class PenumpangApiModel {
   String? id;
   String? name;
@@ -6,24 +8,23 @@ class PenumpangApiModel {
   String? noTelp;
 
   PenumpangApiModel({
-    required this.id,
-    required this.name,
-    required this.password,
-    required this.email,
-    required this.noTelp,
-  });
+    
+    required this.id, 
+    required this.name, 
+    required this.password, 
+    required this.email , 
+    required this.noTelp});
 
-  factory PenumpangApiModel.fromJson(Map<String, dynamic> json) =>
-      PenumpangApiModel(
-        id: json["id"],
-        name: json["name"],
-        password: json["password"],
-        email: json["email"],
-        noTelp: json["noTelp"],
-      );
-
-  @override
-  String toString() {
-    return 'PenumpangApiModel{id: $id, name: $name, email: $email, noTelp: $noTelp}';
+  factory PenumpangApiModel.fromJson(Map<String, dynamic>json){
+    final data = json['data'];
+    return PenumpangApiModel(id: data["id"].toString(), name: data["name"].toString(), password: json["password"], email: json["email"], noTelp: json["noTelp"]);
   }
 }
+
+List<PenumpangApiModel> penumpangApiModelFromJson(String str) {
+  final List<dynamic> jsonData = jsonDecode(str);
+  return jsonData.map((json) => PenumpangApiModel.fromJson(json)).toList();
+}
+
+
+// String todoApiModelToJson(List<PenumpangApiModel>data)=> json.encode(List<dynamic>.from(data.map((x) => x.tojson())));
